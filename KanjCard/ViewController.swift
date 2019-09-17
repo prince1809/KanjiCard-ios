1e043105009964051ddecbf1af18f701ba9cd3e0
prince <prince18pat@gmail.com>
2019-09-17 21:41:35 +0900

    Empty project created

---
+++ KanjCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KanjCard
+//
+//  Created by Prince Kumar on 2019/09/17.
+//  Copyright © 2019 Prince Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
